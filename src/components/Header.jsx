4a6ed316e5ecefa4564d341c036167713ad42f6f
jitendra <jitendrasaini352jjnu@gmail.com>
2023-01-29 13:47:24 +0530

    added icons in header and styled in header and cart sec

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {AiOutlineShoppingCart} from 'react-icons/ai';
+import {AiFillHome} from 'react-icons/ai';
+
 
 const Header = () => {
   const cartdata = useSelector((state)=>state.cart)
   return (
-    <div style={{ display: "flex", alignItems: "center" ,justifyContent :'space-between' }}>
+    <div style={{ display: "flex", alignItems: "center" ,justifyContent :'space-between',margin:'0px 15px' }}>
       <span className="logo">REDUX STORE</span>
       <div>
         <Link className="navLink" to={"/"}>
-          Home
+        <span className="headerIcon">
+         <AiFillHome/>
+        </span>
         </Link>
         <Link className="navLink" to={"/cart"}>
-          Cart
+          
+          <span className="headerIcon">
+          <AiOutlineShoppingCart />
+          </span>
         </Link>
       {/* here i want to show the cart data from the redux store for that i am going to use useSelector method*/}
         <span className="cartCount"> Cart Items : {cartdata.length}</span>
